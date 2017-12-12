5cf5e88bbaf803ce3030b144449662da367c3e50
Quentin <quentin.pouvreau17@gmail.com>
2017-12-12 18:04:18 +0100

    Sematic adaptation

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import os
 import smoothing
 import semantic
 
-smoothing.correctCorpus("Corpus_Apprentissage/corpus_apprentissage_07-12-2017_09h25.txt")
+smoothing.correctCorpus("Corpus_Apprentissage/corpus_apprentissage_14_11_2017_07h51.txt")
+smoothing.majUnigrams()
+smoothing.majBigrams()
 semantic.filterBadTweets("correctedCorpus.txt")
-os.system("cat badTweets.txt | MElt -L -T > melt_badword.melt")
+'''os.system("cat badTweets.txt | MElt -L -T > melt_badword.melt")'''
 
 
 
