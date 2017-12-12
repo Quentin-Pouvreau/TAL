563a9a32aee199e2de482eb1600d0ebf1b4703f3
Quentin <quentin.pouvreau17@gmail.com>
2017-12-12 15:34:07 +0100

    Smoothing update and unused data suppression

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import os
-import tweepy
-import re
-import time
 import smoothing
 import semantic
-import sys
 
 smoothing.correctCorpus("Corpus_Apprentissage/corpus_apprentissage_07-12-2017_09h25.txt")
 semantic.filterBadTweets("correctedCorpus.txt")
